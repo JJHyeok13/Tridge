e66e7a5492bacaf4664020623e13a3a74566eb6c
JJHyeok13 <jjinh99@naver.com>
2025-06-20 00:46:22 +0900

    remove: 사용하지 않는 util 함수 제거

--- src/utils/extractIdFromUrl.ts
+++
@@ -1,4 +0,0 @@
-export const extractIdFromUrl = (url: string) => {
-  const segments = url.split("/").filter(Boolean);
-  return segments[segments.length - 1]; // 마지막 ID
-};
--- src/utils/getPokemonId.ts
+++
@@ -1,4 +0,0 @@
-export function getPokemonIdFromUrl(url: string): string | null {
-  const match = url.match(/\/pokemon\/(\d+)\/?$/);
-  return match ? match[1] : null;
-}
